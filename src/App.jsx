e5a5b0643e75e85781d69874c38d071dc16e3b94
Angel <ANGEL.MARY@kalvium.community>
2025-02-12 10:33:30 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Angel Mary Sajan</h1>
       <p>Email: angel.mary.s69@kalvium.community</p>
-      <p>Phone: +91 8296343030</p>
+      <p>Phone: +91 82963430367880</p>
     </div>
   );
 }
