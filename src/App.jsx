1e1905671a16123d1f463c457326d67aa0c9ef47
Angel <ANGEL.MARY@kalvium.community>
2025-02-12 10:30:23 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Angel Mary Sajan</h1>
+      <p>Email: angel.mary.s69@kalvium.community</p>
+      <p>Phone: +91 8296343030</p>
+    </div>
+  );
+}
+
+export default App;
